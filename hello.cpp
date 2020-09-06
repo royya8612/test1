7aa7ed939308338f7844c0c64e3715416dcbff5b
royya8612 <royya8612@gmail.com>
2020-09-07 01:47:22 +0530

    hello sum

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
 
+int get_sum(int number)
+{
+	if(number/10 == 0)
+		return number;
+	int sum = 0;
+	while(number > 0)
+	{
+		sum+= number%10;
+		number/= 10;
+	}
+	return get_sum(sum);
+	
+}
+
+
 int main()
 {
-	std::cout << "hello" << std::endl;
+	int num, sum;
+	std::cout << "enter a number: \t";
+	std::cin >> num;
+	sum = get_sum(num);
+	std::cout << "sum of digits of your number is: \t" << sum << std::endl;
 	return 0;
 }
 
